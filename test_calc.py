3d647e5f2cb94901773ce9a1ea5e43715245e31b
saamtran <71792958+saamtran@users.noreply.github.com>
2021-05-28 17:48:40 -0700

    test cases for all functions in the calculator app

---
+++ test_calc.py
@@ -0,0 +1,20 @@
+"""
+
+test for calc app
+
+"""
+
+import calc 
+
+class TestCalcApp:
+    def testAdd(self):
+        assert 5 == calc.addition(3,2)
+
+    def testSubtraction(self):
+        assert 10 == calc.subtraction(20,10)
+
+    def testMultiplication(self):
+        assert 50 == calc.multiplication(25, 2)
+
+    def testDivision(self):
+        assert 25 == calc.division(125, 5)
